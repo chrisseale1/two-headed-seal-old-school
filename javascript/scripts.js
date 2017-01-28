6dc967daf99d420fe703f38230a5fb998d1aaa13
Chris Seale <sealechris@gmail.com>
2017-01-27 21:00:42 -0600

    minor change

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,18 +4,18 @@ let toolListRight = document.getElementsByClassName("right");
 
 javascriptExample.addEventListener("click", function(){
     const answer = prompt("This prompt was created using Javascript. Are you now satisfied we know Javascript? Y/n: ");
-    if (answer.toLowerCase() == "n") {
+    if (answer.toLowerCase() == "n" || answer.toLocaleLowerCase() == "no") {
         let answerTwo = prompt("Sigh.  Fine.  What about now?  Y/n: ");
-        if (answerTwo.toLowerCase() == "n") {
+        if (answerTwo.toLowerCase() == "n" || answerTwo.toLocaleLowerCase() == "no") {
             let answerThree;
             do {
                 answerThree = prompt("Look, I can do this all day long.  So, why don't you knock it off and just go ahead and say yes already, okay?  Y/n: ");
-            } while (answerThree.toLowerCase() == "n") alert("And I thank you for your support");
+            } while (answerThree.toLowerCase() == "n" || answerThree.toLocaleLowerCase() == "no") alert("And I thank you for your support");
         } else {
             alert("About time. Thank you for your cooperation and vote of confidence. I'm wasn't sure I could've kept it up forever.");
         }
     } else {
-        alert("Thanks for the vote of confidence.  You're a real mensch!");
+        alert("Thanks for the vote of confidence.  You're a real trooper!");
     }
 });
 
